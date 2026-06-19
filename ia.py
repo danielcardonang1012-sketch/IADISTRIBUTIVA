5749f8adf52a10c597de255ebbc93697da8dec47
danielcardonang1012-sketch <danielcardona.ng1012@gmail.com>
2026-06-18 19:26:08 -0500

    1

--- ia.py
+++ ia.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # Número de drones
 n_drones = 10
@@ -8,14 +9,11 @@ n_drones = 10
 drones = np.zeros((n_drones, 2))
 
 def formar_circulo(radio=5):
-    """Distribuye los drones en un círculo"""
     angulos = np.linspace(0, 2*np.pi, n_drones, endpoint=False)
     return np.column_stack((radio*np.cos(angulos), radio*np.sin(angulos)))
 
 def formar_cuadrado(lado=8):
-    """Distribuye los drones en un cuadrado"""
     puntos = []
-    # 4 lados del cuadrado
     for i in range(n_drones):
         if i < n_drones/4:
             puntos.append([i*(lado/(n_drones/4)), 0])
@@ -25,21 +23,45 @@ def formar_cuadrado(lado=8):
             puntos.append([lado-(i-n_drones/2)*(lado/(n_drones/4)), lado])
         else:
             puntos.append([0, lado-(i-3*n_drones/4)*(lado/(n_drones/4))])
-    return np.array(puntos) - lado/2  # centrar
+    return np.array(puntos) - lado/2
 
-# Formaciones
-pos_circulo = formar_circulo()
-pos_cuadrado = formar_cuadrado()
+def formar_triangulo(lado=8):
+    puntos = []
+    for i in range(n_drones):
+        if i < n_drones/3:
+            puntos.append([i*(lado/(n_drones/3)), 0])
+        elif i < 2*n_drones/3:
+            puntos.append([lado - (i-n_drones/3)*(lado/(n_drones/3)), lado])
+        else:
+            puntos.append([0 + (i-2*n_drones/3)*(lado/(n_drones/3)), (lado/2)])
+    return np.array(puntos) - lado/2
 
-# Visualización
-fig, axs = plt.subplots(1, 2, figsize=(10,5))
+# Diccionario de órdenes
+ordenes = {
+    "circulo": formar_circulo(),
+    "cuadrado": formar_cuadrado(),
+    "triangulo": formar_triangulo()
+}
 
-axs[0].scatter(pos_circulo[:,0], pos_circulo[:,1], color="blue")
-axs[0].set_title("Formación en círculo")
-axs[0].axis("equal")
+# Orden inicial
+orden_actual = "circulo"
+objetivo = ordenes[orden_actual]
 
-axs[1].scatter(pos_cuadrado[:,0], pos_cuadrado[:,1], color="red")
-axs[1].set_title("Formación en cuadrado")
-axs[1].axis("equal")
+# Configuración de la figura
+fig, ax = plt.subplots()
+scat = ax.scatter(drones[:,0], drones[:,1], color="blue")
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
+ax.set_title("Formación de drones")
+ax.axis("equal")
+
+def update(frame):
+    global drones, objetivo
+    # Movimiento suave hacia la formación objetivo
+    drones += 0.1 * (objetivo - drones)
+    scat.set_offsets(drones)
+    return scat,
+
+ani = animation.FuncAnimation(fig, update, frames=200, interval=50, blit=True)
 
 plt.show()
